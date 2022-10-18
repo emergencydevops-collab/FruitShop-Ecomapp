7b2126ec693ca535a250e8c6858313b29fb67bb4
Toluwalase Abigail <toluabby12@gmail.com>
2022-10-18 08:35:49 +0100

    removed build from gitignore

---
+++ client/build/precache-manifest.092f42812739ad825518673f35ac4ac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2188644567dfdb24e39cf8bfdd03ac10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08956372889258992d25",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "92d8e58e0aef058704e3",
+    "url": "/static/js/2.9e8ffbfc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9e8ffbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08956372889258992d25",
+    "url": "/static/js/main.bc06215e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
